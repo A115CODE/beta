07d9b99373e9468372dcd094a305cc99749eb0da
A115CODE <142469086+A115CODE@users.noreply.github.com>
2024-10-01 11:56:55 -0600

    Create reset.js

---
+++ system/reset/reset.js
@@ -0,0 +1,34 @@
+// reset.js
+const url = 'https://xqkozqxrvoypqmbewsvp.supabase.co';
+
+const key =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhxa296cXhydm95cHFtYmV3c3ZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI4NzIyOTUsImV4cCI6MjAzODQ0ODI5NX0.LZ1rbBgmyRatXcavq_oQuqqxucfLXn_WtMYnJC7zys4';
+
+const database = supabase.createClient(url, key);
+
+document
+  .getElementById('reset_password_form')
+  .addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+
+    try {
+      // Solicita un enlace de restablecimiento de contraseña
+      const { error } = await database.auth.resetPasswordForEmail(email, {
+        redirectTo:
+          'http://test-ricoh.infinityfreeapp.com/system/restore/restore.html',
+      });
+
+      if (error) throw error;
+
+      alert(
+        'Revisa tu correo para continuar el restablecimiento de la contraseña.'
+      );
+
+      // Puedes redirigir al usuario o mostrar un mensaje de éxito aquí
+    } catch (error) {
+      console.error('Error:', error.message);
+      alert('Hubo un error al restablecer la contraseña.' + error.message);
+    }
+  });
