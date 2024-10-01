d78fa16878c9a53febadc9ee8887ce507e2bfa86
A115CODE <142469086+A115CODE@users.noreply.github.com>
2024-10-01 12:02:29 -0600

    Create launch.js

---
+++ system/dashboard/components/navbar/launch.js
@@ -0,0 +1,21 @@
+// LAUNCH
+function display(btn, app) {
+  const APLICATION = document.getElementById(app);
+
+  // Ocultar todas las aplicaciones al inicio
+  APLICATION.style.display = 'none';
+
+  // Agregar listener al botón para mostrar/ocultar solo la aplicación correspondiente
+  document.getElementById(btn).addEventListener('click', () => {
+    if (APLICATION.style.display === 'block') {
+      APLICATION.style.display = 'none';
+    } else {
+      APLICATION.style.display = 'block';
+    }
+  });
+}
+
+// Llamada a la función display para cada botón y aplicación
+display('SOLVER_BTN', 'SOLVER_APP');
+display('TASK_BTN', 'TASK_APP');
+display('TONER_BTN', 'TONER_APP');
