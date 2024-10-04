4db22ddf8e1760082b7d27effa28fa98fedb907d
A115CODE <142469086+A115CODE@users.noreply.github.com>
2024-10-04 10:19:07 -0600

    Create lock.js

---
+++ system/dashboard/components/lock/lock.js
@@ -0,0 +1,13 @@
+alert('movile detected');
+
+const MOBILE_DETECTED = document.createElement('div');
+MOBILE_DETECTED.id = 'MOBILE_DETECTED';
+
+MOBILE_DETECTED.innerHTML = `
+ 
+  <img src="..../../assets/error.svg" />  
+
+  <h2>No soportado</h2>
+
+`;
+document.body.appendChild(MOBILE_DETECTED);
