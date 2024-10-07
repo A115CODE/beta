95215d3a8a41a2be308e7d56353563bfd3f5f5bd
A115CODE <jonataneleazarmc@gmail.com>
2024-10-07 10:07:02 -0600

    update index and lock add

---
+++ script.js
@@ -0,0 +1,11 @@
+const LOCK_DETECT = document.createElement('div');
+LOCK_DETECT.id = 'LOCK_DETECT';
+
+LOCK_DETECT.innerHTML = `
+ 
+  <img src="/system/dashboard/assets/lock.svg" />  
+
+  <h2>No soportado!!!</h2>
+
+`;
+document.body.appendChild(LOCK_DETECT);
