47b80fc9761e23cb2e10f44da25152e0ec647c8a
A115CODE <jonataneleazarmc@gmail.com>
2024-10-23 11:07:19 -0600

    update

--- system/restore/restore.js
+++ system/restore/restore.js
@@ -1,22 +1,31 @@
 const url = 'https://xqkozqxrvoypqmbewsvp.supabase.co';
-const key = 'tu_api_key';
+const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhxa296cXhydm95cHFtYmV3c3ZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI4NzIyOTUsImV4cCI6MjAzODQ0ODI5NX0.LZ1rbBgmyRatXcavq_oQuqqxucfLXn_WtMYnJC7zys4';
 
 const database = supabase.createClient(url, key);
 
-document.getElementById('update-password-form').addEventListener('submit', async (event) => {
-    event.preventDefault();
+document.addEventListener('DOMContentLoaded', async () => {
+  const { data: { session }, error } = await database.auth.getSession();
 
-    const newPassword = document.getElementById('new-password').value;
+  if (error || !session) {
+      alert('¡Sesión de autenticación faltante o inválida!');
+      return;
+  }
 
-    try {
-        const { error } = await database.auth.updateUser({ password: newPassword });
+  // El usuario está autenticado, puedes continuar con el restablecimiento de la contraseña.
+  document.getElementById('update-password-form').addEventListener('submit', async (event) => {
+      event.preventDefault();
 
-        if (error) throw error;
+      const newPassword = document.getElementById('new-password').value;
 
-        alert('Tu contraseña ha sido actualizada exitosamente.');
-        // Redirigir o mostrar un mensaje adicional si es necesario
-    } catch (error) {
-        console.error('Error:', error.message);
-        alert('Hubo un error al actualizar la contraseña: ' + error.message);
-    }
+      try {
+          const { error } = await database.auth.updateUser({ password: newPassword });
+
+          if (error) throw error;
+
+          alert('Tu contraseña ha sido actualizada exitosamente.');
+      } catch (error) {
+          console.error('Error:', error.message);
+          alert('Hubo un error al actualizar la contraseña: ' + error.message);
+      }
+  });
 });
