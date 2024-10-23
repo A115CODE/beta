1fe1ed8bafbffc1b557b7ffe7420538e0daaa740
A115CODE <jonataneleazarmc@gmail.com>
2024-10-23 10:55:54 -0600

    updates test to restore

--- system/restore/restore.js
+++ system/restore/restore.js
@@ -1,53 +1,22 @@
-// restore.js
-
-// Primero, verificamos si el token está en los parámetros de consulta
-let accessToken = new URLSearchParams(window.location.search).get(
-  'access_token'
-);
-
-// Si no lo encontramos, lo buscamos en el fragmento de la URL
-if (!accessToken) {
-  const hashParams = new URLSearchParams(window.location.hash.substring(1));
-  accessToken =
-    hashParams.get('access_token') ||
-    hashParams.get('token') ||
-    hashParams.get('auth_token');
-}
-
 const url = 'https://xqkozqxrvoypqmbewsvp.supabase.co';
-const key =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhxa296cXhydm95cHFtYmV3c3ZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI4NzIyOTUsImV4cCI6MjAzODQ0ODI5NX0.LZ1rbBgmyRatXcavq_oQuqqxucfLXn_WtMYnJC7zys4';
+const key = 'tu_api_key';
 
 const database = supabase.createClient(url, key);
 
-document
-  .getElementById('reset-password-form')
-  .addEventListener('submit', async (event) => {
+document.getElementById('update-password-form').addEventListener('submit', async (event) => {
     event.preventDefault();
 
     const newPassword = document.getElementById('new-password').value;
 
-    if (!accessToken) {
-      alert('Token de acceso no encontrado en la URL.');
-      return;
-    }
-
     try {
-      // Establece la nueva contraseña utilizando el token de acceso
-      const { error } = await database.auth.verifyOTP({
-        token: accessToken,
-        type: 'recovery',
-        password: newPassword,
-      });
+        const { error } = await database.auth.updateUser({ password: newPassword });
 
-      if (error) throw error;
+        if (error) throw error;
 
-      alert(
-        'Contraseña restablecida con éxito. Ahora puedes iniciar sesión con tu nueva contraseña.'
-      );
-      window.location.href = '/login.html'; // Redirige al login después de restablecer la contraseña
+        alert('Tu contraseña ha sido actualizada exitosamente.');
+        // Redirigir o mostrar un mensaje adicional si es necesario
     } catch (error) {
-      console.error('Error al restablecer la contraseña:', error.message);
-      alert('Hubo un error al restablecer la contraseña.');
+        console.error('Error:', error.message);
+        alert('Hubo un error al actualizar la contraseña: ' + error.message);
     }
-  });
+});
