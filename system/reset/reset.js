88a8743aaf54f150900937abd4cbe99fef3812be
A115CODE <jonataneleazarmc@gmail.com>
2024-10-24 14:44:17 -0600

    updates

--- system/reset/reset.js
+++ system/reset/reset.js
@@ -15,7 +15,7 @@ document
 
     try {
       // Solicita un enlace de restablecimiento de contraseña
-      const { error } = await database.auth.resetPasswordForEmail(email, {
+      const { error } = await database.auth.reset_password_for_email(email, {
         redirectTo:
           'https://a115code.github.io/ricoh/system/restore/restore.html',
       });
